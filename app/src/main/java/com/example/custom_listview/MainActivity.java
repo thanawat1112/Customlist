d0b61b8b8996d85ee56b4c8b2caea9c3215ab4c1
thanawat1112 <59079111+thanawat1112@users.noreply.github.com>
2022-01-20 00:44:06 +0700

    submit

---
+++ app/src/main/java/com/example/custom_listview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.custom_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+
+import com.example.custom_listview.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int[] imageId = {R.drawable.a,R.drawable.b,R.drawable.c};
+        String[] name = {"Thanawat","thanakorn","natdanai"};
+        String[] id = {"623410033-9","623410034-7","623410049-4"};
+        String[] age = {"21","21","21"};
+        String[] phoneNo = {"088-0637398","064-3469907","061-0953083"};
+        String[] country = {"Nongkhai","Buriram","Nongbualampuu"};
+        String[] universi = {"Khonkean University","Khonkean University","Khonkean University"};
+
+        ArrayList<User> userArrayList = new ArrayList<>();
+
+        for(int i = 0;i< imageId.length;i++){
+
+            User user = new User(name[i],id[i],age[i],phoneNo[i],country[i],imageId[i]);
+            userArrayList.add(user);
+
+        }
+
+
+        ListAdapter listAdapter = new ListAdapter(MainActivity.this,userArrayList);
+
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(MainActivity.this,UserActivity.class);
+                i.putExtra("name",name[position]);
+                i.putExtra("phone",phoneNo[position]);
+                i.putExtra("country",country[position]);
+                i.putExtra("universi",universi[position]);
+                i.putExtra("imageid",imageId[position]);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
